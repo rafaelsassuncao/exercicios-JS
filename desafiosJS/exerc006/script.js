b08b674a394cb34748333017b37f9b7b2cec87aa
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-01-29 18:43:32 -0300

    Realizado primeiro exercicio conforme solicitado no README, curso completo de JS.

---
+++ desafiosJS/exerc006/script.js
@@ -0,0 +1,10 @@
+var nome = "Rafael";
+var idade = 32;
+var comidaFavorita = "",
+    comidaFavorita = "Massa";
+
+console.log(nome, idade, comidaFavorita);
+
+var sobreNome, esposa, pets, pais, profissao;
+
+console.log(sobreNome, esposa, pets, pais, profissao);
