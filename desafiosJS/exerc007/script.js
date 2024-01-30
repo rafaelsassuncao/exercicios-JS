8f62280da32e50f257e1ab4ac566100e09c46a16
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-01-30 06:38:36 -0300

    exercicio 007 concluido sem consultar a aula do professor, exercicio sobre variaveis.

---
+++ desafiosJS/exerc007/script.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+var nome = "Rafael";
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = "1991";
+
+// Declare uma variável com a sua idade
+var idade = 32;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobreNome = "Santos Assunção";
+var nomeCompleto = `${nome} ${sobreNome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
