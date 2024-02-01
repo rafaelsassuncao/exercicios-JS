889a1430ba9c669b4a1d2c7d8b1db46ba19e8c57
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-01 07:27:45 -0300

    Exercicio onde o professor ensina que tudo é objeto, tambem teve o exercicio sobre arrays e loops.

---
+++ desafiosJS/exerc012 - tudo-e-objeto/script.js
@@ -0,0 +1,17 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = "Rafael";
+
+//charAt;
+//endsWith;
+//includes;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn");
+
+//.addEventListener
+//.appendChild
+//.before
+//.after
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
