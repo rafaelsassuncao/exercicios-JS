22cc4e76fcfe3fb4bd2bf07bd696f45931c5c587
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-03 15:47:22 -0300

    acresceita os os comentarios dizendo o que foi feito para resolver, cada desafio.

--- desafiosJS/exerc015 - escopo/script.js
+++ desafiosJS/exerc015 - escopo/script.js
@@ -1,4 +1,5 @@
 // Por qual motivo o código abaixo retorna com erros?
+//Nesse casso foi necessário aonde console.log(*var* > por cor, marca, portas);
 {
     var cor = "preto";
     const marca = "Fiat";
@@ -19,8 +20,10 @@ console.log(somarDois(5));
 console.log(dividirDois(10));
 
 // O que fazer para total retornar 500?
+//A solução foi transformar o var em const
 const numero = 50;
 
+//A solução foi transformar a var em let, pois dai o mesmo conseguiu coletar os valores fora do escopo//
 for (let numero = 0; numero < 10; numero++) {
     console.log(numero);
 }
