516d0575d4604ef08ff7ff699682201a09e27dd4
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-11 20:03:21 -0300

    exercicio 007 concluido

--- desafiosJS/exerc022 - desafio006-animaisfantasticos/script.js
+++ desafiosJS/exerc022 - desafio006-animaisfantasticos/script.js
@@ -1,7 +1,20 @@
 // Duplique o menu e adicione ele em copy
 
+const menu = document.querySelector(".menu");
+const copy = document.querySelector(".copy");
+
+const cloneMenu = menu.cloneNode(true);
+copy.appendChild(menu);
+
 // Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector(".faq");
+const primeiroDt = faq.querySelector("dt");
 
 // Selecione o DD referente ao primeiro DT
 
+const proximoDd = primeiroDt.nextElementSibling;
+
 // Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector(".animais");
+
+faq.innerHTML = animais.innerHTML;
