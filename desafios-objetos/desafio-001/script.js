c44324e357e6b0bb9dd9a27d46e9a70124a7e890
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-14 10:33:57 -0300

    a terceita parte do exercicio infelizmente não consegui resolver sozinho

--- desafios-objetos/desafio-001/script.js
+++ desafios-objetos/desafio-001/script.js
@@ -7,8 +7,25 @@ const pessoa = {
     },
 };
 
+//Constructor Function, utilizando os dados acima:
+class Pessoa {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.andar = function () {
+            console.log(this.nome + " andou ");
+        };
+    }
+}
+
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
+const joao = new Pessoa("João", 20);
+joao.andar();
+const maria = new Pessoa("Maria", 25);
+maria.andar();
+const bruno = new Pessoa("Bruno", 15);
+bruno.andar();
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -17,3 +34,20 @@ const pessoa = {
 // elements, retorna NodeList com os elementos selecionados
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
+
+function Dom(seletor) {
+    const elementList = document.querySelectorAll(seletor);
+    this.elements = elementList;
+    this.addClass = function (classe) {
+        elementList.forEach((element) => {
+            element.classList.add(classe);
+        });
+    };
+    this.removeClass = function (classe) {
+        elementList.forEach((element) => {
+            element.classList.remove(classe);
+        });
+    };
+}
+const listaItens = new Dom("li");
+const ul = new Dom("ul");
