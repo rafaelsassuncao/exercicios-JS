93e0992798f707020d9f335f168f09087b416536
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-21 16:10:16 -0300

    Exercicio concluido, primeira parte da linha 3 a 19 foi mais tranquilo de resolver sozinho, ja a segunda parte precisei ver o professor executando.

--- desafios-objetos/desafio-002/script.js
+++ desafios-objetos/desafio-002/script.js
@@ -1,22 +1,40 @@
 // Crie uma função construtora de Pessoas
 // Deve conter nome, sobrenome e idade
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
+
+Pessoa.prototype.nomeCompleto = function () {
+    return `${this.nome} ${this.sobrenome}`;
+};
+
+const rafael = new Pessoa("Rafael", "Santos Assunção", 33);
+
+console.log(rafael.nomeCompleto());
 
 // Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
 
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLAllCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
+
 // Liste os construtores dos dados abaixo
 const li = document.querySelector("li");
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li; // HTMLLIELement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; //Number
+li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; // String
