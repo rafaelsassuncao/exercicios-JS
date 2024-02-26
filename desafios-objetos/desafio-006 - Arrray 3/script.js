ec10e339ee07f601b0044e061704f02c9f34073b
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-26 11:56:23 -0300

    Exercicio 1 da linha 5 a linha 14 eu resolvi praticamente sozinho. Parece muito simples, mas fiquei bem feliz.

--- desafios-objetos/desafio-006 - Arrray 3/script.js
+++ desafios-objetos/desafio-006 - Arrray 3/script.js
@@ -1,8 +1,17 @@
-const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
 // Remova o primeiro valor de comidas e coloque em uma variável
 // Remova o último valor de comidas e coloque em uma variável
 // Adicione 'Arroz' ao final da array
 // Adicione 'Peixe' e 'Batata' ao início da array
+const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
+const primeiraComida = comidas.shift();
+const ultimaComida = comidas.pop();
+
+comidas.splice(0, 0, "Peixe", "Batata");
+comidas.splice(5, 4, "Arroz");
+
+console.log(primeiraComida);
+console.log(ultimaComida);
+console.log(comidas);
 
 const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
 // Arrume os estudantes em ordem alfabética
