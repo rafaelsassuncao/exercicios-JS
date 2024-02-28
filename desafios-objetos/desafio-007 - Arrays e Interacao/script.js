da4c2a158183e4bc3ed0fc83f00f6643836ba381
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-02-28 16:27:01 -0300

    os exercicios da linha 09 a 14, 19 a 25. Eu consegui resolver praticamente só consultando o material da aula.

--- desafios-objetos/desafio-007 - Arrays e Interacao/script.js
+++ desafios-objetos/desafio-007 - Arrays e Interacao/script.js
@@ -2,13 +2,27 @@
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
 
+const cursos = document.querySelectorAll(".curso");
+
+console.log(cursos);
+
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maiorQue100 = numeros.filter((numeros) => numeros > 100);
+
+console.log(maiorQue100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
+
 const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+const temBaixo = instrumentos.some((instrumento) => {
+    return instrumento === "Baixo";
+});
+
+console.log(temBaixo);
+
 // Retorne o valor total das compras
 const compras = [
     {
