3d208e6983a0251b5a6bbc86dfd80d5254db1436
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-03-21 07:47:49 -0300

    exerc 028  - commit int

---
+++ desafiosJS/exerc028 - setTimeout e setInterval 2/script.js
@@ -0,0 +1,9 @@
+// // Mude a cor da tela para azul e depois para vermelho a cada 2s.
+// function mudarClasse() {
+//     document.body.classList.toggle("active");
+// }
+
+// setInterval(mudarClasse, 2000);
+
+// Crie um cronometro utilizando o setInterval. Deve ser possível
+// iniciar, pausar e resetar (duplo clique no pausar).
