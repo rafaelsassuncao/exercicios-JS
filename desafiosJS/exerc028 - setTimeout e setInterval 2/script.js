484b7882cc483698561591417226476eeffc898e
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-03-21 15:07:51 -0300

    eu fiz igual a aula conforme o professor orientou no exercicio e mesmo assim não funcionou, fiz  a comparacao com o codigo do professor e estava igual. Acho que era algum bug no editor.

--- desafiosJS/exerc028 - setTimeout e setInterval 2/script.js
+++ desafiosJS/exerc028 - setTimeout e setInterval 2/script.js
@@ -9,31 +9,29 @@
 // iniciar, pausar e resetar (duplo clique no pausar).
 
 const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar')
-const tempo = document.querySelector('.tempo')
+const pausar = document.querySelector('.pausar');
+const tempo = document.querySelector('.tempo');
 
-iniciar.addEventListener('click', iniciarTempo)
-pausar.addEventListener('click', pausarTempo)
-pausar.addEventListener('dblclick', resetarTempo)
-
+iniciar.addEventListener('click', iniciarTempo);
+pausar.addEventListener('click', pausarTempo);
+pausar.addEventListener('dblclick', resetarTempo);
 
 let i = 0;
 let timer;
 
-
 function iniciarTempo() {
-    timer = setInterval(() => {
-        tempo.innerText = i++;
-    }, 100);
-    iniciar.setAttribute('disabled', '');
+  timer = setInterval(() => {
+    tempo.innerText = i++;
+  }, 100);
+  iniciar.setAttribute('disabled', '');
 }
 
 function pausarTempo() {
-    clearInterval(timer);
-    iniciar.removeAttribute('disabled');
+  clearInterval(timer);
+  iniciar.removeAttribute('disabled');
 }
 
 function resetarTempo() {
-    tempo.innerText = 0;
-    i = 0;
+  tempo.innerText = 0;
+  i = 0;
 }
