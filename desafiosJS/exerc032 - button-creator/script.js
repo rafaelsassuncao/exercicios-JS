78aee76e072895284b79d3dc60558436fb46afa4
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-03-26 13:59:01 -0300

    ajuste finais na aula sobre lLocal Storage

--- desafiosJS/exerc032 - button-creator/script.js
+++ desafiosJS/exerc032 - button-creator/script.js
@@ -1,61 +1,63 @@
-const controles = document.getElementById("controles");
-const css = document.querySelector(".css");
-const btn = document.querySelector(".btn");
+const controles = document.getElementById('controles');
+const cssText = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+controles.addEventListener('change', handleChange);
 
-controles.addEventListener("change", handleChage);
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem';
+  },
+}
 
-const handStyle = {
-    element: btn,
-    backgroundColor(value) {
-        this.element.style.backgroundColor = value;
-    },
-    height(value) {
-        this.element.style.height = value + "px";
-    },
-    width(value) {
-        this.element.style.width = value + "px";
-    },
-    texto(value) {
-        this.element.innerText = value;
-    },
-    color(value) {
-        this.element.style.color = value;
-    },
-    border(value) {
-        this.element.style.border = value;
-    },
-    borderRadius(value) {
-        this.element.style.borderRadius = value + "px";
-    },
-    fontFamily(value) {
-        this.element.style.fontFamily = value;
-    },
-    fontSize(value) {
-        this.element.style.fontSize = value + "rem";
-    },
-};
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
 
-function handleChage(event) {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    handStyle[name](value);
-    saveValues(name, value);
-    showCss();
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
 }
 
 function saveValues(name, value) {
-    localStorage[name];
+  localStorage[name] = value;
 }
 
 function setValues() {
-    const properties = Object.keys(localStorage);
-    properties.forEach((propertie) => {
-        controles.element[propertie];
-    });
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+  showCss();
 }
 
+setValues();
+
 function showCss() {
-    css.innerText =
-        "<span>" + btn.style.cssText.split("; ").join(";</span><span>");
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
 }
