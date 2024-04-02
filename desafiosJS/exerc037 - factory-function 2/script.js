8da649ece5c88100d21425da2aaf7d81868c7f5c
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-04-02 10:03:09 -0300

    Aula sobre factory function 2 concluida

--- desafiosJS/exerc037 - factory-function 2/script.js
+++ desafiosJS/exerc037 - factory-function 2/script.js
@@ -1,51 +1,58 @@
 function $$(selectedElements) {
-    const elements = document.querySelectorAll(selectedElements);
-  
-    function on(onEvent, callback) {
-      elements.forEach(element => {
-        element.addEventListener(onEvent, callback);
-      });
-      return this; // retornar this irá funcionar da mesma forma
-    }
-  
-    function hide() {
-      elements.forEach(element => {
-        element.style.display = 'none';
-      });
-      return this;
-    }
-  
-    function show() {
-      elements.forEach(element => {
-        element.style.display = 'initial';
-      });
-      return this;
-    }
-  
-    function addClass(className) {
-      elements.forEach(element => {
-        element.classList.add(className);
-      });
-      return this;
-    }
-  
-    function removeClass(className) {
-      elements.forEach(element => {
-        element.classList.add(className);
-      });
-      return this;
-    }
-    
-    return Object.freeze({
-      elements,
-      on,
-      hide,
-      show,
-      addClass,
-      removeClass,
+  const elements = document.querySelectorAll(selectedElements);
+
+  function hide() {
+    elements.forEach(element => {
+      element.style.display = 'none';
     });
+    return this;
   }
-  
-  const buttons = $$('button');
-  buttons.hide().show().addClass('ativo').removeClass('ativo');
-  
+
+  function show() {
+    elements.forEach(element => {
+      element.style.display = 'initial';
+    });
+    return this;
+  }
+
+  function on(onEvent, callback) {
+    elements.forEach(element => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this;
+  }
+
+  function addClass(className) {
+    elements.forEach(element => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+
+  function removeClass(className) {
+    elements.forEach(element => {
+      element.classList.remove(className);
+    });
+    return this;
+  }
+
+  return {
+    elements,
+    hide,
+    show,
+    on,
+    addClass,
+    removeClass,
+  }
+}
+
+const btns = $$('button');
+
+console.log(btns.hide().show());
+
+function handleClick(event) {
+  console.log(event.target);
+  btns.addClass('active');
+}
+
+btns.on('click', handleClick);
